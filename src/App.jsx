6cd834a02e87ee439b452c07717b2cc38ea0884b
Nugar <cladeadenugar@gmail.com>
2023-12-15 14:54:03 +0100

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./App.css";
+// import { BrowswerRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import ProfilePage from "./Pages/ProfilePage";
 
 const App = () => {
-  return <div className="app">App</div>;
+  return <div className="app">Apdhdhncncn</div>;
 };
 
 export default App;
