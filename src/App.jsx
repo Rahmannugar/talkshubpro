2eb5286639c79bd165a2a8632289d47b0a851bbe
Nugar <cladeadenugar@gmail.com>
2023-12-16 04:55:25 +0100

    added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,15 @@ import LoginPage from "./Pages/LoginPage";
 import ProfilePage from "./Pages/ProfilePage";
 
 const App = () => {
-  return <div className="app">Apdhdhncncn</div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
